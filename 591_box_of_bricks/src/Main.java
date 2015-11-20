eaae665977957a660cf4343418d8952702194a9b
Annika Magnusson <annika.h.magnusson@gmail.com>
2015-11-20 14:18:16 +0100

    Still not ok with UVa

--- 591_box_of_bricks/src/Main.java
+++ 591_box_of_bricks/src/Main.java
@@ -1,60 +1,72 @@
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 class Main {
-    static int set;
-    static String output = "";
     static BufferedReader br;
 
     public static void main (String args[]) throws IOException {
+        String data = "6\n" +
+                "5 5 5 5 5 7\n" +
+                "4\n" +
+                "2 2 2 1\n" +
+                "10\n" +
+                "100 100 100 100 100 100 100 100 100 -100\n" +
+                "5\n" +
+                "-45 -23 -2 5 -100\n" +
+                "6\n" +
+                "100 -100 100 -100 100 -100\n" +
+                "0";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
         br = new BufferedReader(new InputStreamReader(System.in));
 
-        set = 1;
+        Main main = new Main();
+        main.moveBricks();
 
-        moveBricks();
+        br.close();
+        System.exit(0);
     }
 
-    static void moveBricks() throws IOException {
+    void moveBricks() throws IOException {
+        int set = 1;
         //Check the number of stacks
         int numberOfStacks = Integer.parseInt(br.readLine());
 
-        // If line is 0 then program should end
-        if (numberOfStacks == 0){
-            System.out.print(output.substring(0, output.length() - 1));
-            return;
-        }
-
-        // Read the line with stacks
-        String bricksInStacks = br.readLine();
-
-        // Split the line into stacks
-        String[] stacks = bricksInStacks.split(" ");
-
-        int moves = 0;
-
-        int totalNumberOfBricks = 0;
-
-        for (int e = 0; e < numberOfStacks; e++) {
-            int bricksInStack = Integer.parseInt(stacks[e]);
-            totalNumberOfBricks += bricksInStack;
-
-        }
-
-
-
-        int wallHeight = totalNumberOfBricks/numberOfStacks;
-
-        for(int i = 0; i < numberOfStacks; i++) {
-            int bricksInStack = Integer.parseInt(stacks[i]);
-
-            if (bricksInStack > wallHeight) {
-                moves += bricksInStack-wallHeight;
+        while (numberOfStacks != 0) {
+            if (set != 1) {
+                System.out.println("");
             }
-        }
+            // Read the line with stacks
+            String bricksInStacks = br.readLine();
 
-        output += "Set #" + set + "\nThe minimum number of moves is " + moves + ".\n" + "\n";
-        set ++;
-        moveBricks();
+            // Split the line into stacks
+            String[] stacks = bricksInStacks.split(" ");
+
+            int moves = 0;
+
+            int totalNumberOfBricks = 0;
+
+            for (int e = 0; e < numberOfStacks; e++) {
+                int bricksInStack = Integer.parseInt(stacks[e]);
+                totalNumberOfBricks += bricksInStack;
+
+            }
+
+            int wallHeight = totalNumberOfBricks/numberOfStacks;
+
+            for(int i = 0; i < numberOfStacks; i++) {
+                int bricksInStack = Integer.parseInt(stacks[i]);
+
+                if (bricksInStack > wallHeight) {
+                    moves += bricksInStack-wallHeight;
+                }
+            }
+
+            System.out.println("Set #" + set);
+            System.out.println("The minimum number of moves is " + moves);
+            set ++;
+            numberOfStacks = Integer.parseInt(br.readLine());
+        }
     }
 }
